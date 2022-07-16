50de2aefc4de467e5abb59ed6bc5379f1966cc4e
Recep Çelimli <receply13@hotmail.com>
2022-07-16 22:28:42 +0300

    added

---
+++ Route/config.js
@@ -0,0 +1,7 @@
+app.config(function ($routeProvider) {
+    $routeProvider
+    .when("/",{temlateUrl:"index.html"})
+    .when("/red",{temlateUrl:"red.html"})
+    .when("/green",{temlateUrl:"green.html"})
+    
+});
